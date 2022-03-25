7554eab7d11295dae6f363fb019adad7b57ca671
Kushan Dias <55612561+Kushan-Dias@users.noreply.github.com>
2022-03-25 22:49:24 +0530

    delete directory
    delete. directory

--- portfolio/index.js
+++
@@ -1,9 +0,0 @@
-var  logo = document.querySelectorAll("#logo path");
-logo.forEach(function(i){
-  i.style.strokeDasharray = i.getTotalLength()+"px";
-  i.style.strokeDashoffset = i.getTotalLength()+"px";
-});
-
-setTimeout(function(){
-  window.location.href = 'home.html';
-}, 9000);
